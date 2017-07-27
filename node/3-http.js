d93957a97bc3ed178861a108db70f7344a8831f0
gabi <gabib@live.com>
2017-07-26 21:07:14 -0700

    complete node js problem 3
    Signed-off-by: gabi <gabib@live.com>

--- node/3-http.js
+++ node/3-http.js
@@ -2,21 +2,17 @@
 
 var http = require('http'); // do not change this line
 
-
 var server = http.createServer(function(req, res) {
 
-  // http://localhost:8080/missing should return a status code 404 with 'your princess is in another castle' in plain text
   if (req.url === '/missing') {
     res.writeHead(404, {
       'Content-Type': 'text/plain'
     });
     res.write('your princess is in another castle');
     res.end();
-
-  // http://localhost:8080/redirect should redirect the request to '/redirected' by using 302 as the status code
   } else if (req.url === '/redirect') {
     res.writeHead(302, {
-      'Location': 'http://localhost:8080/redirected'
+      'Location': '/redirected'
     });
     res.end();
   } else if (req.url === '/redirected') {
@@ -24,23 +20,44 @@ var server = http.createServer(function(req, res) {
       'Content-Type': 'text/plain'
     });
     res.end();
-
-  // http://localhost:8080/cache should return 'cache this resource' in plain text and set the cache max age to a day
   } else if (req.url === '/cache') {
     res.writeHead(200, {
-      'Cache-Control': 'max-age=1',
+      'Cache-Control': 'max-age=86400',
       'Content-Type': 'text/plain'
     });
     res.write('cache this resource');
     res.end();
-}
-
+  } else if (req.url === '/cookie') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'Set-Cookie': 'hello=world'
+    });
+    res.write('i gave you a cookie');
+    res.end();
+  } else if (req.url === '/check') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
+    if (req.headers.cookie) {
+        res.write('yes');
+      } else {
+        res.write('no');
+      }
+    // console.log(req.headers.cookie);
+    res.end();
+  } else {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
+    res.end();
+  }
 });
 
 server.listen(process.env.PORT || 8080);
 
 
-
+// http://localhost:8080/missing should return a status code 404 with 'your princess is in another castle' in plain text
+// http://localhost:8080/redirect should redirect the request to '/redirected' by using 302 as the status code
+// http://localhost:8080/cache should return 'cache this resource' in plain text and set the cache max age to a day
 // http://localhost:8080/cookie should return 'i gave you a cookie' in plain text and set 'hello=world' as a cookie
-
 // http://localhost:8080/check should return 'yes' / 'no' in plain text depending on whether the browser has the 'hello' cookie
