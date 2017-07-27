3a61665a62a2197a56374811938c3811edccecdb
gabi <gabib@live.com>
2017-07-26 19:55:54 -0700

    first 3 exercises
    Signed-off-by: gabi <gabib@live.com>

--- node/2-url.js
+++ node/2-url.js
@@ -4,6 +4,45 @@ var http = require('http'); // do not change this line
 var url = require('url'); // do not change this line
 var querystring = require('querystring'); // do not change this line
 
+
+var server = http.createServer(function(req, res) {
+  if (req.url === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('you have accessed the root');
+    res.end();
+  } else if (req.url.indexOf('/test/') === 0) {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('you have accessed \"' + decodeURIComponent(req.url.substr(6)) +
+      '\" within test');
+    res.end();
+  } else if (url.parse(req.url, true).pathname === '/attributes') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    var q = url.parse(req.url, true).query;
+    var h = '<!DOCTYPE html><html><body><table border="1">';
+    for (var key in q){
+      h += '<tr><td>';
+      h += key;
+      h += '</td><td>';
+      h += q[key];
+      h += '</td></tr>';
+    }
+    h += '</table></body></html>';
+    res.write(h);
+    res.end();
+  } else {
+    res.end()
+  }
+});
+
+
+server.listen(process.env.PORT || 8080);
+
 // http://localhost:8080/ should return 'you have accessed the root' in plain text
 
 // http://localhost:8080/test/hello should return 'you have accessed "hello" within test' in plain text
--- node/3-http.js
+++ node/3-http.js
@@ -2,11 +2,44 @@
 
 var http = require('http'); // do not change this line
 
-// http://localhost:8080/missing should return a status code 404 with 'your princess is in another castle' in plain text
 
-// http://localhost:8080/redirect should redirect the request to '/redirected' by using 302 as the status code
+var server = http.createServer(function(req, res) {
 
-// http://localhost:8080/cache should return 'cache this resource' in plain text and set the cache max age to a day
+  // http://localhost:8080/missing should return a status code 404 with 'your princess is in another castle' in plain text
+  if (req.url === '/missing') {
+    res.writeHead(404, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('your princess is in another castle');
+    res.end();
+
+  // http://localhost:8080/redirect should redirect the request to '/redirected' by using 302 as the status code
+  } else if (req.url === '/redirect') {
+    res.writeHead(302, {
+      'Location': 'http://localhost:8080/redirected'
+    });
+    res.end();
+  } else if (req.url === '/redirected') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.end();
+
+  // http://localhost:8080/cache should return 'cache this resource' in plain text and set the cache max age to a day
+  } else if (req.url === '/cache') {
+    res.writeHead(200, {
+      'Cache-Control': 'max-age=1',
+      'Content-Type': 'text/plain'
+    });
+    res.write('cache this resource');
+    res.end();
+}
+
+});
+
+server.listen(process.env.PORT || 8080);
+
+
 
 // http://localhost:8080/cookie should return 'i gave you a cookie' in plain text and set 'hello=world' as a cookie
 
